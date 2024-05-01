93452547249277fdf469a338ec53482aaf8256b6
MiguelAngelCelayaA <maca@gmail.com>
2024-05-01 09:18:36 -0600

    Practica 2 react

---
+++ src/MovieSearchApp.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const MovieSearchApp = () => {
+    
+  return (
+    <div className='container'>
+        <h1>Buscador de Peliculas</h1>
+        <form>
+        <input type="text" />
+        <button>Buscar</button>
+        </form>
+    </div>
+  )
+}
